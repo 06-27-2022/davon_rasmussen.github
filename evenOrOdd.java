8252143ff6c6a4b72ef436d9434f828979bdaa88
davonrasmussen <108144757+davonrasmussen@users.noreply.github.com>
2022-07-11 10:06:04 -0400

    Add files via upload
    Even or Odd Challenge

---
+++ evenOrOdd.java
@@ -0,0 +1,23 @@
+package Revature_Work.evenOrOdd;
+
+import java.util.Scanner;
+
+public class evenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int num = scan.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+            scan.close();
+        }
+
+    }
+
+}
